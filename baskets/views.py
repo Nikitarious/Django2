1182f57cc27e231ae7a4b742b6a2f83da888b698
Nikitarius <nikitanirvana11@gmail.com>
2021-07-14 16:07:42 +0300

    login_required

--- baskets/views.py
+++ baskets/views.py
@@ -7,6 +7,7 @@ from products.models import Product
 from baskets.models import Basket
 
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -21,12 +22,14 @@ def basket_add(request, product_id):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_edit(request, id, quantity):
     if request.is_ajax():
         basket = Basket.objects.get(id=id)
